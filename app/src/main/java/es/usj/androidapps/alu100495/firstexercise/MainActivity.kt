9e846502c3099e97ca52848bb6eb27ecf2ebf0d9
TestAndroid <5jitibor@gmail.com>
2021-02-25 18:58:56 +0100

    Version 1.1

--- app/src/main/java/es/usj/androidapps/alu100495/firstexercise/MainActivity.kt
+++ app/src/main/java/es/usj/androidapps/alu100495/firstexercise/MainActivity.kt
@@ -1,25 +1,72 @@
 package es.usj.androidapps.alu100495.firstexercise
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var tvVisible = false;
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        var number = 0;
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView.visibility= TextView.INVISIBLE
-        button.setOnClickListener{
-            toggleVisibility()
-
+        textView.setText(number.toString())
+        //textView.visibility= TextView.VISIBLE
+        plus1.setOnClickListener{
+            number++
+            textView.setText(number.toString())
         }
-    }
-    fun toggleVisibility(){
-        val visibility = if(textView.visibility== TextView.VISIBLE) {
-            textView.visibility= TextView.INVISIBLE
-        }else textView.visibility= TextView.VISIBLE
+       plus2.setOnClickListener{
+            number+= 2
+            textView.setText(number.toString())
+       }
+        plus3.setOnClickListener{
+            number+= 3
+            textView.setText(number.toString())
+        }
+        less1.setOnClickListener{
+            number--
+            textView.setText(number.toString())
+        }
+        less2.setOnClickListener{
+            number-= 2
+            textView.setText(number.toString())
+        }
+        less3.setOnClickListener{
+            number-= 3
+            textView.setText(number.toString())
+        }
+
+        multiply2.setOnClickListener{
+            number*=2
+            textView.setText(number.toString())
+        }
+        multiply3.setOnClickListener{
+            number*= 3
+
+            textView.setText(number.toString())
+        }
+        multiply4.setOnClickListener{
+            number*= 4
+            textView.setText(number.toString())
+        }
+        multiplyNegative2.setOnClickListener{
+            number*= -2
+            textView.setText(number.toString())
+        }
+        multiplyNegative3.setOnClickListener{
+            number*= -3
+            textView.setText(number.toString())
+        }
+        multiplyNegative4.setOnClickListener{
+            number*= -4
+            textView.setText(number.toString())
+        }
+
+       reset.setOnClickListener{
+           number=0
+           textView.setText(number.toString())
+       }
     }
 }
 
