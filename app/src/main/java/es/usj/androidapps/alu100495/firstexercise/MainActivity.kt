0c5cd58dcb3d09171d695e9e987287ce308d8685
TestAndroid <5jitibor@gmail.com>
2021-02-25 16:32:20 +0100

    Version 1.0

--- app/src/main/java/es/usj/androidapps/alu100495/firstexercise/MainActivity.kt
+++ app/src/main/java/es/usj/androidapps/alu100495/firstexercise/MainActivity.kt
@@ -1,11 +1,25 @@
 package es.usj.androidapps.alu100495.firstexercise
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var tvVisible = false;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.visibility= TextView.INVISIBLE
+        button.setOnClickListener{
+            toggleVisibility()
+
+        }
+    }
+    fun toggleVisibility(){
+        val visibility = if(textView.visibility== TextView.VISIBLE) {
+            textView.visibility= TextView.INVISIBLE
+        }else textView.visibility= TextView.VISIBLE
     }
 }
+
